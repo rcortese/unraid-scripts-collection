ca35f890a46f3c8556cdccc497d5cc5025efe247
rcortese <23123652+rcortese@users.noreply.github.com>
2021-12-07 17:15:06 -0300

    refactor

--- alternate_vms.sh
+++ alternate_vms.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
+set -e
 
 # Fill with the names of VMs to alternate between
-VM0_NAME="Pop_OS"
-VM1_NAME="Windows 10"
+# If a VM is listed as active, the next one will be started (or first if last)
+declare -r -a vms_to_alternate=(  "Windows 10" "Pop_OS" )
+
 
 # Switches between two vms
 # $1 - name of vm to shutdown
 # $2 - name of vm to start
 switch_vm() {
+
   local -r vm_to_shutdown="$1"; shift
   local -r vm_to_start="$1"; shift
-  virsh shutdown "$vm_to_shutdown"
+
+  echo "requesting shutdown of VM: ${vm_to_shutdown}"
+  virsh shutdown "${vm_to_shutdown}"
   sleep 30
-  virsh start "$vm_to_start"
+  echo "requesting start of VM: ${vm_to_start}"
+  virsh start "${vm_to_start}"
 }
 
-if virsh list | grep -q "$VM0_NAME"; then
-  switch_vm "$VM0_NAME" "$VM1_NAME"
-else
-  switch_vm "$VM1_NAME" "$VM0_NAME"
-fi
+
+main() {
+
+  # vm_to_start defaults to first one
+  local vm_to_start="${vms_to_alternate[0]}"
+  # vm_to_shutdown defaults to a display message
+  local vm_to_shutdown="NO_ACTIVE_VM_FOUND"
+
+  echo "searching for an active VM on the following list:
+  "
+  local -r active_vms_list=$(virsh list)
+  echo "${active_vms_list}
+  "
+  for i in "${!vms_to_alternate[@]}"
+  do
+    # if vm is listed as active
+    if echo "${active_vms_list}" | grep -q "${vms_to_alternate[i]}"; then
+      echo "${vms_to_alternate[i]} found active!"
+      # set it to shutdown
+      vm_to_shutdown="${vms_to_alternate[i]}"
+      # and the next one to start (if not last)
+      if ! [ -z "${vms_to_alternate[i+1]}" ]; then
+        vm_to_start="${vms_to_alternate[i+1]}"
+      fi
+      echo "next on list: ${vm_to_start}"
+    fi
+  done
+
+  switch_vm "${vm_to_shutdown}" "${vm_to_start}"
+  exit 0
+}
+
+main
