8aaaee984382216e71954d91ca71c8742517bd86
rcortese <23123652+rcortese@users.noreply.github.com>
2021-12-07 01:18:41 -0300

    Evaluate if one VM is on and start the other

--- alternate_vms.sh
+++ alternate_vms.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sleep 30
-virsh start "Pop_OS"
+VM0_NAME="Pop_OS"
+VM1_NAME="Windows 10"
+
+if virsh list | grep -q "$VM0_NAME"; then
+  sleep 30
+  virsh start $VM1_NAME
+elif virsh list | grep -q "$VM1_NAME"; then
+  sleep 30
+  virsh start $VM0_NAME
+fi
