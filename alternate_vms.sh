6fc21f8a534cf01b8edb8ac9adb360b161f2b193
rcortese <23123652+rcortese@users.noreply.github.com>
2021-12-07 01:42:27 -0300

    function switch_vms

--- alternate_vms.sh
+++ alternate_vms.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Fill with the names of VMs to alternate between
 VM0_NAME="Pop_OS"
 VM1_NAME="Windows 10"
 
+# Switches between two vms
+# $1 - name of vm to shutdown
+# $2 - name of vm to start
+switch_vm() {
+  sleep 30
+  virsh start "$2"
+}
+
 if virsh list | grep -q "$VM0_NAME"; then
-  sleep 30
-  virsh start $VM1_NAME
+  switch_vms "$VM0_NAME" "$VM1_NAME"
 elif virsh list | grep -q "$VM1_NAME"; then
-  sleep 30
-  virsh start $VM0_NAME
+  switch_vms "$VM1_NAME" "$VM0_NAME"
 fi
